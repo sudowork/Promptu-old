97fabba0e9e64df29e003d925dfec7283795b559
Brandon Millman <brandon.millman@gmail.com>
2012-07-21 15:05:58 -0700

    ios: added some open source libraries, set up constants

---
+++ ios/PromptuApp/Constants.h
@@ -0,0 +1,26 @@
+//
+//  Constants.h
+//  PromptuApp
+//
+//  Created by Brandon Millman on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+// Colors
+
+#define COLOR_TEXT [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:1.0]
+#define COLOR_TEXT_LIGHT [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.3f]
+
+// Fonts
+
+#define FONT_BOLD [UIFont fontWithName:@"HelveticaNeue-Bold" size:16]
+#define FONT_LIGHT [UIFont fontWithName:@"HelveticaNeue-Light" size:14]
+
+// Prompt View Setup
+
+#define PROMPT_BACKGROUND @"gplaypattern.png"
+#define PROMPT_ANIM_SPEED 0.3
+
+// Notification Center
+
+#define MENU_ITEM_SELECTED @"MENU_ITEM_SELECTED"
