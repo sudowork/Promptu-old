e41e1260b61120e342b3f525cc2d8798c756f06c
Yang Su <suyang1+services@gmail.com>
2012-07-22 12:02:38 -0700

    added code to generate prompts

--- web/public/js/view/adminview.js
+++ web/public/js/view/adminview.js
@@ -4,6 +4,35 @@
 		template: PUApp.templates['admin-template'],
 		initialize: function () {
 		},
+		events: {
+			'click button[type="submit"]': 'submitPrompt'
+		},
+		submitPrompt: function () {
+			var head = $('#header').val(),
+				body = $('#body').val(),
+				channels = [],
+				date = new Date()/1000 + 10;
+				author = '500c21f5a8762bad30000001',
+				group = '500c238803c67d0d31000004';
+			if ($('input[value="apn"]').attr('checked')) {
+				channels.push('apn');
+			}
+			if ($('input[value="email"]').attr('checked')) {
+				channels.push('email');
+			}
+			if ($('input[value="sms"]').attr('checked')) {
+				channels.push('checked');
+			}
+			this.createPrompt({
+				sessionToken: PUApp.user.token,
+				group: group,
+				channels:channels,
+				header: head,
+				body: body,
+				author: author,
+				duedate: date
+			});
+		},
 		createPrompt: function (obj) {
 			$.ajax({
 				url: 'promptuapp.com:3000/prompt/create',
